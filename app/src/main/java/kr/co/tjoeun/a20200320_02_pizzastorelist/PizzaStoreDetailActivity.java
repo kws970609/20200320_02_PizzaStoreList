88124ab1086d4cc07364892d9c23d9692f6d6382
kws970609 <kws970609@users.noreply.github.com>
2020-03-20 15:45:28 +0900

    전화 걸기 권한 획득  - TedPermission 라이브러리 활용

--- app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/PizzaStoreDetailActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/PizzaStoreDetailActivity.java
@@ -3,9 +3,18 @@ package kr.co.tjoeun.a20200320_02_pizzastorelist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 import kr.co.tjoeun.a20200320_02_pizzastorelist.databinding.ActivityMainBinding;
 import kr.co.tjoeun.a20200320_02_pizzastorelist.databinding.ActivityPizzaStoreDetailBinding;
@@ -13,6 +22,7 @@ import kr.co.tjoeun.a20200320_02_pizzastorelist.datas.PizzaStore;
 
 public class PizzaStoreDetailActivity extends BaseActivity {
     ActivityPizzaStoreDetailBinding binding = null;
+    PizzaStore store =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +35,48 @@ public class PizzaStoreDetailActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                가게의 전화번호를 => Call 액션을 연결.
+//                사용자의 허가가 OK인 상황에서 실행하도록.
+//                => tedPermission 라이브러리 활용
+                PermissionListener pl = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+//                        허가가 떨어진 상황. => 전화 걸기 코드 실험.
+                        String phoneNumUri = String.format("tel:%s", store.getPhoneNum());
+
+                        Uri uri = Uri.parse("tel:전화번호"); // 띄어쓰기 or - 들어가지 않게.
+                        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+//                      권한을 사용자가 거부한 상황. => 전화권한을 허가해야 사용가능합니다. Toast
+                        Toast.makeText(mContext,"전화권한을 사용해야 이용가능합니다.",Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+//                onClick 실행? 사용자가 손으로 클릭할때 실행.
+//                권한 상항 대응 실행? 코드로 권한 요청을 직접날려서 실행.
+                TedPermission.with(mContext).setPermissionListener(pl)
+                        .setDeniedMessage("설정에서 허용해줘야사용 가능합니다.")
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+
+
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
 
-        PizzaStore store =(PizzaStore) getIntent().getSerializableExtra("store");
+         store =(PizzaStore) getIntent().getSerializableExtra("store");
 
         binding.storeNameTxt.setText(store.getStoreName());
         binding.storePhoneTxt.setText(store.getPhoneNum());
