1323c8dd7070ce647ffa8050ca911deb2c25028f
kws970609 <kws970609@users.noreply.github.com>
2020-03-20 11:47:17 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/MainActivity.java
@@ -0,0 +1,18 @@
+package kr.co.tjoeun.a20200320_02_pizzastorelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.a20200320_02_pizzastorelist.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
